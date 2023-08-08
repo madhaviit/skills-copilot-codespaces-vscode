494ed9b948dd8ec3bd9625854907c9016176d254
Madhav Kadam <94967156+madhaviit@users.noreply.github.com>
2023-08-08 13:06:48 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember(){
+    var member = {
+        name: "John Doe",
+        age: 25,
+        skills: ["HTML", "CSS", "JS"],
+        calculateNumbers: function(var1, var2){
+            return var1 + var2;
+        }
+    }
+    return member;
+
+}
